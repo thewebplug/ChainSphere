5e4418a087fcdb6516e567df5dd1a06e35329ee7
spomaria <spomariae@gmail.com>
2024-05-16 16:18:46 +0100

    Resolved bugs from previous merge request

--- src/VRFv2Consumer.sol
+++ src/VRFv2Consumer.sol
@@ -2,108 +2,108 @@
 // An example of a consumer contract that relies on a subscription for funding.
 pragma solidity ^0.8.7;
 
-import {VRFCoordinatorV2Interface} from "@chainlink/contracts@1.1.0/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
-import {VRFConsumerBaseV2} from "@chainlink/contracts@1.1.0/src/v0.8/vrf/VRFConsumerBaseV2.sol";
-import {ConfirmedOwner} from "@chainlink/contracts@1.1.0/src/v0.8/shared/access/ConfirmedOwner.sol";
+// import {VRFCoordinatorV2Interface} from "@chainlink/contracts@1.1.0/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+// import {VRFConsumerBaseV2} from "@chainlink/contracts@1.1.0/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+// import {ConfirmedOwner} from "@chainlink/contracts@1.1.0/src/v0.8/shared/access/ConfirmedOwner.sol";
 
 
-contract VRFv2Consumer is VRFConsumerBaseV2, ConfirmedOwner {
-    event RequestSent(uint256 requestId, uint32 numWords);
-    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+// contract VRFv2Consumer is VRFConsumerBaseV2, ConfirmedOwner {
+//     event RequestSent(uint256 requestId, uint32 numWords);
+//     event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
-    struct RequestStatus {
-        bool fulfilled; // whether the request has been successfully fulfilled
-        bool exists; // whether a requestId exists
-        uint256[] randomWords;
-    }
-    mapping(uint256 => RequestStatus)
-        public s_requests; /* requestId --> requestStatus */
-    VRFCoordinatorV2Interface COORDINATOR;
+//     struct RequestStatus {
+//         bool fulfilled; // whether the request has been successfully fulfilled
+//         bool exists; // whether a requestId exists
+//         uint256[] randomWords;
+//     }
+//     mapping(uint256 => RequestStatus)
+//         public s_requests; /* requestId --> requestStatus */
+//     VRFCoordinatorV2Interface COORDINATOR;
 
-    // Your subscription ID.
-    uint64 s_subscriptionId;
+//     // Your subscription ID.
+//     uint64 s_subscriptionId;
 
-    // past requests Id.
-    uint256[] public requestIds;
-    uint256 public lastRequestId;
+//     // past requests Id.
+//     uint256[] public requestIds;
+//     uint256 public lastRequestId;
 
-    bytes32 keyHash =
-        0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
+//     bytes32 keyHash =
+//         0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
 
   
-    uint32 callbackGasLimit = 100000;
+//     uint32 callbackGasLimit = 100000;
 
-    // The default is 3, but you can set this higher.
-    uint16 requestConfirmations = 3;
+//     // The default is 3, but you can set this higher.
+//     uint16 requestConfirmations = 3;
 
-    // For this example, retrieve 2 random values in one request.
-    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
-    uint32 numWords = 1;
+//     // For this example, retrieve 2 random values in one request.
+//     // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+//     uint32 numWords = 1;
 
-    /**
-     * HARDCODED FOR SEPOLIA
-     * COORDINATOR: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
-     */
-    constructor(
-        uint64 subscriptionId
-    )
-        VRFConsumerBaseV2(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625)
-        ConfirmedOwner(msg.sender)
-    {
-        COORDINATOR = VRFCoordinatorV2Interface(
-            0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
-        );
-        s_subscriptionId = subscriptionId;
-    }
+//     /**
+//      * HARDCODED FOR SEPOLIA
+//      * COORDINATOR: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+//      */
+//     constructor(
+//         uint64 subscriptionId
+//     )
+//         VRFConsumerBaseV2(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625)
+//         ConfirmedOwner(msg.sender)
+//     {
+//         COORDINATOR = VRFCoordinatorV2Interface(
+//             0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+//         );
+//         s_subscriptionId = subscriptionId;
+//     }
 
-    // Assumes the subscription is funded sufficiently.
-    function requestRandomWords()
-        external
-        onlyOwner
-        returns (uint256 requestId)
-    {
-        // Will revert if subscription is not set and funded.
-        requestId = COORDINATOR.requestRandomWords(
-            keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
-        );
-        s_requests[requestId] = RequestStatus({
-            randomWords: new uint256[](0),
-            exists: true,
-            fulfilled: false
-        });
-        requestIds.push(requestId);
-        lastRequestId = requestId;
-        emit RequestSent(requestId, numWords);
-        return requestId;
-    }
+//     // Assumes the subscription is funded sufficiently.
+//     function requestRandomWords()
+//         external
+//         onlyOwner
+//         returns (uint256 requestId)
+//     {
+//         // Will revert if subscription is not set and funded.
+//         requestId = COORDINATOR.requestRandomWords(
+//             keyHash,
+//             s_subscriptionId,
+//             requestConfirmations,
+//             callbackGasLimit,
+//             numWords
+//         );
+//         s_requests[requestId] = RequestStatus({
+//             randomWords: new uint256[](0),
+//             exists: true,
+//             fulfilled: false
+//         });
+//         requestIds.push(requestId);
+//         lastRequestId = requestId;
+//         emit RequestSent(requestId, numWords);
+//         return requestId;
+//     }
 
-    function fulfillRandomWords(
-        uint256 _requestId,
-        uint256[] memory _randomWords
-    ) internal override {
+//     function fulfillRandomWords(
+//         uint256 _requestId,
+//         uint256[] memory _randomWords
+//     ) internal override {
 
-        require(s_requests[_requestId].exists, "request not found");
-        s_requests[_requestId].fulfilled = true;
-        s_requests[_requestId].randomWords = _randomWords;
+//         require(s_requests[_requestId].exists, "request not found");
+//         s_requests[_requestId].fulfilled = true;
+//         s_requests[_requestId].randomWords = _randomWords;
 
-        emit RequestFulfilled(_requestId, _randomWords);
-    }
+//         emit RequestFulfilled(_requestId, _randomWords);
+//     }
 
-    function getRequestStatus(
-        uint256 _requestId
-    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
-        require(s_requests[_requestId].exists, "request not found");
-        RequestStatus memory request = s_requests[_requestId];
-        return (request.fulfilled, request.randomWords );
-    }
-    function getRandomNumber(uint256 num) public view returns (uint256) {
-     require(s_requests[lastRequestId].exists && s_requests[lastRequestId].fulfilled, "Request not fulfilled");
-     uint256 randomWords = s_requests[lastRequestId].randomWords[0]; // Assuming there's only one random value
-     return randomWords % num;
-}
+//     function getRequestStatus(
+//         uint256 _requestId
+//     ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+//         require(s_requests[_requestId].exists, "request not found");
+//         RequestStatus memory request = s_requests[_requestId];
+//         return (request.fulfilled, request.randomWords );
+//     }
+//     function getRandomNumber(uint256 num) public view returns (uint256) {
+//      require(s_requests[lastRequestId].exists && s_requests[lastRequestId].fulfilled, "Request not fulfilled");
+//      uint256 randomWords = s_requests[lastRequestId].randomWords[0]; // Assuming there's only one random value
+//      return randomWords % num;
+// }
 
-}
+// }
